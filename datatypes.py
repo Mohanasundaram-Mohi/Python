b017bad2ab582a71df753de2763f3b875446fbc8
Mohanasundaram Selvaraj <168515064+Mohanasundaram-Mohi@users.noreply.github.com>
2024-12-31 23:40:39 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,112 @@
+emp_id = 1
+name = "Mohan"
+salary = 1000.56
+
+print("Employee id is:", emp_id)
+print("Employee name is:", name)
+print("Employee salary is:", salary)
+
+print(type(emp_id))
+print(type(name))
+print(type(salary))
+
+value = 10000.56
+
+print(value)
+print(type(value))
+
+a = True
+b = False
+
+print(a, type(a))
+print(b, type(b))
+
+
+salary = 10000.56
+print(salary)
+print(type(salary))
+
+a = None
+
+print(a)
+print(type(a))
+
+name= None
+name = "Mohan"
+print("name is", name)
+print(type(name))
+
+
+name1 = 'Mohi'
+
+name2 = "Naresh"
+
+name3 = '''Palani'''
+name4 = """Prasad"""
+
+
+print(name1)
+print(name2)
+print(name3)
+print(name4)
+
+print(type(name1))
+print(type(name2))
+print(type(name3))
+print(type(name4))
+
+print ("List Data structure")
+a = 10
+b = 20.34
+c = [30, 40, 50, "Mohan"]
+
+print(a)
+print(b)
+print(c)
+
+a = 10
+b = 20.34
+c = [30, 40.67, "Mohan"]
+
+print(a)
+print(b)
+print(c)
+print()
+print(type(a))
+print(type(b))
+print(type(c))
+
+print("Tuple data Structure")
+
+a = [10, 20.34, "Do"]
+b = (10, 20.34, "Dom")
+
+print(a)
+print(b)
+print()
+print(type(a))
+print(type(b))
+
+print("set")
+
+a = [10, 20, 30, 40]
+b = (10, 20, 30, 40)
+c = {10, 20, 30, 40}
+
+print(a)
+print(b)
+print(c)
+
+d = {101: "Daniel", 102: "Phani", 103: "Neelima"}
+
+print(d)
+print(type(d))
+
+
+print("FOR/Range data type")
+values=range(10)
+
+for value in values:
+	print(value)
+
+
